9b1be1e8cebb031c6676b78d553abd4bafc112e2
Mrigank Mukherjee <mrigankm20@gmail.com>
2022-10-07 23:12:04 +0530

    Added Color Picker

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+/* 
+if want to change color add a variable here and create a function haivng a button
+Mouse hover to mouse hold
+*/
+
 const con=document.querySelector('.con');
+let color="red";
+const picker=document.querySelector('#color');
+picker.addEventListener('change',function(e){
+    color=e.target.value;
+})
 
 function create(n){
     for(i=0;i<n*n;i++)
@@ -16,9 +26,9 @@ function destroy(n){
 }
 
 const box=document.querySelector('.box');
-con.addEventListener('mouseover',function(e){
+con.addEventListener('mouseover',(e)=>{
     if(e.target.matches('.box'))
-        e.target.style.cssText="background-color:black;"; //if want to change color add a variable here and create a function haivng a button
+        e.target.style.cssText=`background-color:${color};`; 
 });
 
 const reset=document.querySelector('#reset');
